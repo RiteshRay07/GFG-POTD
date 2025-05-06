97f7ac5ec9b3f480fe3ee6d95798079f0b8f6e97
RiteshRay <DeveloperRiteshRay@gmail.com>
2025-05-06 07:18:23 +0530

    left view of BT

---
+++ May 2025/06-05-2025.cpp
@@ -0,0 +1,43 @@
+class Solution {
+    public:
+      vector<int> leftView(Node *root) {
+          
+          vector<int>ans;
+          
+          // base case
+          if(root==NULL){
+              return ans;
+          }
+          
+          // create a queue for store levelorder 
+          queue<pair<Node*, int >>q;
+  
+          
+          int level =1;
+          q.push({root,level});
+          
+          while(!q.empty()){
+              
+              pair<Node*, int> temp = q.front();
+              q.pop();
+              
+              Node* node = temp.first;
+              int currLevel = temp.second;
+              
+              if(currLevel == level){
+                  ans.push_back(node->data);
+                  level++;
+              }
+              
+              if(node->left){
+                  q.push({node->left, level});
+              }
+              if(node->right){
+                  q.push({node->right, level});
+              }
+          }
+          
+          return ans;
+          
+      }
+  };
